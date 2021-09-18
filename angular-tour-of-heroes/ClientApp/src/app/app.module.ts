2acde885b18e019ae402e89e8bc6f3db9f1cc739
Camilo Brito <candresbrito@unicesar.edu.co>
2021-09-17 23:04:56 -0500

    punto 3 terminado

--- angular-tour-of-heroes/ClientApp/src/app/app.module.ts
+++ angular-tour-of-heroes/ClientApp/src/app/app.module.ts
@@ -7,11 +7,13 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
